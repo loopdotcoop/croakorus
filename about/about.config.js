a5080b221a7c70c2e4aa912474f86fd262875422
richplastow <rich.plastow@kandimedia.com>
2014-09-06 21:41:49 +0100

    + desoot@0.0.1-2   ‘about.not-found.html’ only lists project meta in the development environment;

--- about/about.config.js
+++ about/about.config.js
@@ -26,28 +26,32 @@ Config = {
       , widgets: {} // can be filled with widget areas, eg 'footer-left'
 
       , changelog: [
-            '+                        Initial commit'
-          , '+ desoot@0.0.1-1         paste ‘Workflow’ and ‘Changelog’ from looptopia@0.2.16 to ‘README.md’;  \n' +
-            '                         paste ‘.gitignore’ from looptopia@0.2.16;  \n' +
-            '                         `$ git flow init -d` to install git flow with default branch names;  \n' +
-            '                         `$ mrt create tmp/desoot` to install Meteor;  \n' +
-            '                         `$ mv tmp/desoot/.[!.]* tmp/desoot/* ./` to move Meteor files to top level;  \n' +
-            '                         `$ rm -rf tmp; rm desoot.*` to remove unnecessary files;  \n' +
-            '                         change ‘.meteor/release’ from `METEOR@0.9.0.1` to `0.8.3`;  \n' +
-            '                         `$ mrt add iron-router`;  \n' +
-            '                         `$ mrt add handlebars-helpers` to allow Templates to use `{{$...}}`;  \n' +
-            '                         `$ mrt add less`;  \n' +
-            '                         paste ‘about.config.js’ from looptopia@0.2.16 and edit data;  \n' +
-            '                         paste ‘about.helper.js’ from looptopia@0.2.16 and cut most code;  \n' +
-            '                         paste ‘about.less’ from looptopia@0.2.16;  \n' +
-            '                         paste ‘about.not-found.html’ from looptopia@0.2.16; '
-        ]
-      , version:     '0.0.1-1'
+            '+                  Initial commit'
+          , '+ desoot@0.0.1-1   paste ‘Workflow’ and ‘Changelog’ from looptopia@0.2.16 to ‘README.md’;  \n' +
+            '                   paste ‘.gitignore’ from looptopia@0.2.16;  \n' +
+            '                   `$ git flow init -d` to install git flow with default branch names;  \n' +
+            '                   `$ mrt create tmp/desoot` to install Meteor;  \n' +
+            '                   `$ mv tmp/desoot/.[!.]* tmp/desoot/* ./` to move Meteor files to top level;  \n' +
+            '                   `$ rm -rf tmp; rm desoot.*` to remove unnecessary files;  \n' +
+            '                   change ‘.meteor/release’ from `METEOR@0.9.0.1` to `0.8.3`;  \n' +
+            '                   `$ mrt add iron-router`;  \n' +
+            '                   `$ mrt add handlebars-helpers` to allow Templates to use `{{$...}}`;  \n' +
+            '                   `$ mrt add less`;  \n' +
+            '                   paste ‘about.config.js’ from looptopia@0.2.16 and edit data;  \n' +
+            '                   paste ‘about.helper.js’ from looptopia@0.2.16 and cut most code;  \n' +
+            '                   paste ‘about.less’ from looptopia@0.2.16;  \n' +
+            '                   paste ‘about.not-found.html’ from looptopia@0.2.16; '
+          , '+ desoot@0.0.1-2   ‘about.not-found.html’ only lists project meta in the development environment; '
+        ] , version: '0.0.1-2'
     }
 }
 
 //// 'development' on localhost, 'production' on modulus.
-if ('object' === typeof process) { Config.about.env = process.env.NODE_ENV; } // @todo is this ever used?
+if (Meteor.isServer && 'object' === typeof process && process.env) {
+    Config.about.env = process.env.NODE_ENV; // @todo is this ever used?
+} else if (Meteor.isClient && window && window.location) {
+    Config.about.env = '127.0.0.1' === window.location.hostname || 'localhost' === window.location.hostname ? 'development' : 'production'; // @todo make sure this agrees with serverside `Config.about.env` ... Meteor method?
+}
 
 
 Router.configure({
